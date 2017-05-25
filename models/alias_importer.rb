a087c7393bc05befac598751a4163aafed1e3c76
Daniel Ruiz <daniel.ruiz@11paths.com>
2017-05-25 16:53:02 +0200

    Added AliasImporter model

---
+++ models/alias_importer.rb
@@ -0,0 +1,15 @@
+require 'yaml'
+
+class AliasImporter
+
+  attr_reader :alias_file_path
+
+  def initialize(alias_file_path: 'alias.yaml')
+    @alias_file_path = alias_file_path
+  end
+
+  def parse_aliases
+    YAML.load_file alias_file_path
+  end
+
+end
